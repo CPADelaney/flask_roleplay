9835e9de266985ea0b407bd4b91dd05e9ab1088b
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-22 01:11:46 -0800

    Update settings_routes.py

--- routes/settings_routes.py
+++ routes/settings_routes.py
@@ -1,1 +1,17 @@
+# routes/settings_routes.py
+
+from flask import Blueprint, request, jsonify
+from db.connection import get_db_connection
+import random, json
+
+settings_bp = Blueprint('settings_bp', __name__)
+
+@settings_bp.route('/generate_mega_setting', methods=['POST'])
+def generate_mega_setting():
+    # your code
+    ...
+
+@settings_bp.route('/get_current_roleplay', methods=['GET'])
+def get_current_roleplay():
+    ...
 
