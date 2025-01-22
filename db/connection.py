75e1972fa5c050234c67e27a9f61ce061ac49467
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-22 01:10:01 -0800

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,1 +1,11 @@
+# db/connection.py
+
+import os
+import psycopg2
+
+def get_db_connection():
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if not DATABASE_URL:
+        raise EnvironmentError("DATABASE_URL is not set.")
+    return psycopg2.connect(DATABASE_URL, sslmode='require')
 
