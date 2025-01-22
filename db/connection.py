16559f3d8d51ca8cd4cebaf1cc05aa79417b5624
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-22 09:11:55 -0800

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -4,8 +4,11 @@ import os
 import psycopg2
 
 def get_db_connection():
+    """
+    A simple function to open a new psycopg2 connection each time.
+    You can call this in any route or function that needs database access.
+    """
     DATABASE_URL = os.getenv("DATABASE_URL")
     if not DATABASE_URL:
         raise EnvironmentError("DATABASE_URL is not set.")
     return psycopg2.connect(DATABASE_URL, sslmode='require')
-
