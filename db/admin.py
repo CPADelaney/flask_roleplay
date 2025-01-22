ac0de6f31833559dfa6ef678bdaaf49aaea929bf
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-22 11:23:59 -0800

    Update admin.py

--- db/admin.py
+++ db/admin.py
@@ -3,7 +3,8 @@
 from flask import Blueprint, jsonify
 from db.connection import get_db_connection
 from db.initialization import initialize_database
-from logic.stats_logic import insert_stat_definitions, insert_or_update_game_rules, insert_missing_settings
+from logic.stats_logic import insert_stat_definitions, insert_or_update_game_rules
+from routes.settings_routes import insert_missing_settings
 
 admin_bp = Blueprint('admin_bp', __name__)
 
