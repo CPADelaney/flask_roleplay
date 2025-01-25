4e094da2a988e6f18168995e6f84cd38f26a7d8c
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-24 16:02:45 -0800

    Update admin.py

--- db/admin.py
+++ db/admin.py
@@ -5,6 +5,7 @@ from db.connection import get_db_connection
 from db.initialization import initialize_database
 from logic.stats_logic import insert_stat_definitions, insert_or_update_game_rules
 from routes.settings_routes import insert_missing_settings
+from logic.initialization import initialize_all_data
 
 admin_bp = Blueprint('admin_bp', __name__)
 
