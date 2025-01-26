bfe711fedd3efca0c44067df1dd0f87da32d9d0e
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-01-25 23:56:47 -0800

    Create debug.py

---
+++ routes/debug.py
@@ -0,0 +1,9 @@
+@debug_bp.route('/create_npc', methods=['GET'])
+def debug_create_npc():
+    new_id = create_npc(introduced=False)
+    return jsonify({"message": f"Created new NPC with id {new_id}"}), 200
+
+@debug_bp.route('/generate_setting', methods=['GET'])
+def debug_generate_setting():
+    mega_data = generate_mega_setting_logic()
+    return jsonify(mega_data), 200
