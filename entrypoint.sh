518e1d9ca96f508f4660917a351c1c5672f244f9
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-06 10:46:54 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ "$SERVICE_TYPE" = "worker" ]; then
-    echo "Starting Celery Worker..."
-    exec celery -A tasks.celery_app worker --loglevel=info
+    echo "Starting Celery Worker with concurrency=1..."
+    exec celery -A tasks.celery_app worker --loglevel=INFO --concurrency=1
 else
     echo "Starting Web Server..."
     exec gunicorn --bind 0.0.0.0:8080 --timeout 600 --worker-class uvicorn.workers.UvicornWorker main:app
