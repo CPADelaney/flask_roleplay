110f4838ba1630f28badd09b850aca3d89eefde7
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-18 01:19:04 -0800

    Create celery_worker.py

---
+++ celery_worker.py
@@ -0,0 +1,11 @@
+# celery_worker.py
+import eventlet
+eventlet.monkey_patch()  # 1) Done early!
+
+from celery.bin import worker
+from main import celery_app  # 2) Now import your app, which may import tasks, etc.
+
+if __name__ == "__main__":
+    w = worker.worker()
+    # Start Celery with eventlet concurrency:
+    w.run_from_argv(["celery", "-A", "main.celery_app", "worker", "-P", "eventlet", "--loglevel=INFO"])
