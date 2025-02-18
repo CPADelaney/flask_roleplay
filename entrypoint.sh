35397664c5fe765e2c9bad514319246444ed41bb
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-18 09:58:45 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$SERVICE_TYPE" = "worker" ]; then
     echo "Starting Celery Worker with concurrency=1..."
-    exec celery -A tasks.celery_app worker --loglevel=INFO --concurrency=1
+    exec celery -A main.celery_app worker --loglevel=INFO --concurrency=1
 else
     echo "Starting Web Server (Gunicorn + Eventlet)..."
     # IMPORTANT: specify "-k eventlet" and point to your Flask app object
