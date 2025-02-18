180a81dcbfff7e898c851aadac2c0f104615919c
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-18 14:45:22 -0800

    Update and rename celery_config.py to celery_app.py

---
+++ celery_app.py
@@ -0,0 +1,19 @@
+# celery_app.py
+import os
+from celery import Celery
+
+def create_celery_app():
+    RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672//")
+    celery_app = Celery("my_celery_app", broker=RABBITMQ_URL, backend="rpc://")
+    celery_app.conf.update(
+        task_serializer='json',
+        accept_content=['json'],
+        result_serializer='json',
+        timezone='UTC',
+        enable_utc=True,
+        worker_log_format="%(levelname)s:%(name)s:%(message)s",
+        worker_redirect_stdouts_level='INFO'
+    )
+    return celery_app
+
+celery_app = create_celery_app()
