1641769bc8a69f5cebe2eef1e81a120608af2b45
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-18 09:58:03 -0800

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -1,17 +1,17 @@
 # celery_config.py
-import os
-from celery import Celery
+#import os
+#from celery import Celery
 
-BROKER_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost//")
+#BROKER_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost//")
 # Or if you use AMQPS, default to something like:
 # BROKER_URL = os.getenv("RABBITMQ_URL", "amqps://guest:guest@localhost:5671//")
 
-celery_app = Celery('tasks', broker=BROKER_URL, backend='rpc://')
+#celery_app = Celery('tasks', broker=BROKER_URL, backend='rpc://')
 
-celery_app.conf.update(
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-    timezone='UTC',
-    enable_utc=True,
-)
+#celery_app.conf.update(
+#    task_serializer='json',
+#    accept_content=['json'],
+#    result_serializer='json',
+#    timezone='UTC',
+#    enable_utc=True,
+#)
