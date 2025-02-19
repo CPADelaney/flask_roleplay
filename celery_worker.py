7687a8438265a295cb621ba5f59d6f43709998ef
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-19 09:51:08 -0800

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -4,5 +4,4 @@ eventlet.monkey_patch()
 from celery_app import celery_app
 
 if __name__ == "__main__":
-    # Start the Celery worker directly
     celery_app.worker_main(["worker", "--loglevel=INFO", "-P", "eventlet"])
