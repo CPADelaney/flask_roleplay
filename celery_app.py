16b7b844eea5fcfd277b6ea5c18a1b411310114d
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-02-20 09:52:37 -0800

    Update celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -1,25 +1,15 @@
-# celery_app.py
-import os
+# celery_config.py
 from celery import Celery
-import tasks
 
-def create_celery_app():
-    RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672//")
-    celery_app = Celery("my_celery_app", broker=RABBITMQ_URL, backend="rpc://", include=["tasks"])
-    celery_app.conf.update(
-        task_serializer='json',
-        accept_content=['json'],
-        result_serializer='json',
-        timezone='UTC',
-        enable_utc=True,
-        worker_log_format="%(levelname)s:%(name)s:%(message)s",
-        worker_redirect_stdouts_level='INFO',
-        broker_transport_options={
-            'heartbeat': 60,           # Send a heartbeat every 60 seconds
-            'connection_attempts': 3,  # Try to reconnect up to 3 times
-            'retry_delay': 5,          # Wait 5 seconds between attempts
-        },
-    )
-    return celery_app
+# Use RabbitMQ as the broker.
+# The default URL for RabbitMQ (with guest/guest) is: amqp://guest:guest@localhost//
+# For the result backend, you can use RPC (which is free) or choose another backend.
+celery_app = Celery('tasks', broker='amqp://guest:guest@localhost//', backend='rpc://')
 
-celery_app = create_celery_app()
+celery_app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+)
