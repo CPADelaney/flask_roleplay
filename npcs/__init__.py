e518baf6871fa8c7cb54d09a02fd8f1c9a00aa5f
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-03-13 10:24:33 -0700

    Create __init__.py

---
+++ npcs/__init__.py
@@ -0,0 +1,12 @@
+# npcs/__init__.py
+
+from .agent import NPCAgent
+from .coordinator import NPCAgentCoordinator
+from .system import NPCAgentSystem
+from .memory import NPCMemoryManager
+from .decision import NPCDecisionEngine
+from .perception import EnvironmentPerception
+from .relationship import NPCRelationshipManager
+from .creation import NPCCreationHandler
+from .handler import NPCInteractionHandler
+from .behavior import BehaviorEvolution
