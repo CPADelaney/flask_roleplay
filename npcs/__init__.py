3f5327014de82e31684c61fae356b528048b4611
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-03-21 16:22:04 -0700

    Update __init__.py

--- npcs/__init__.py
+++ npcs/__init__.py
@@ -1,12 +1,12 @@
 # npcs/__init__.py
 
-from .agent import NPCAgent
-from .coordinator import NPCAgentCoordinator
-from .system import NPCAgentSystem
-from .memory import NPCMemoryManager
-from .decision import NPCDecisionEngine
-from .perception import EnvironmentPerception
-from .relationship import NPCRelationshipManager
-from .creation import NPCCreationHandler
-from .handler import NPCInteractionHandler
-from .behavior import BehaviorEvolution
+from .npc_agent import NPCAgent
+from .npc_coordinator import NPCAgentCoordinator
+from .npc_agent_system import NPCAgentSystem
+from .npc_memory import NPCMemoryManager
+from .npc_decisions import NPCDecisionEngine
+from .npc_perception import EnvironmentPerception
+from .npc_relationship import NPCRelationshipManager
+from .new_npc_creation import NPCCreationHandler
+from .npc_handler import NPCInteractionHandler
+from .npc_behavior import BehaviorEvolution
