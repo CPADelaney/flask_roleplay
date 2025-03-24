324b895c703b5712867da2f6031110fb6a905828
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-03-24 13:44:56 -0700

    Update __init__.py

--- nyx/core/procedural_memory/__init__.py
+++ nyx/core/procedural_memory/__init__.py
@@ -1,20 +1,31 @@
 # nyx/core/procedural_memory/__init__.py
 
-# Import main components
+# Import core models
 from .models import (
     ActionTemplate, ChunkTemplate, ContextPattern, ChunkPrediction,
     ControlMapping, ProcedureTransferRecord, Procedure, StepResult,
-    ProcedureStats, TransferStats
+    ProcedureStats, TransferStats, HierarchicalProcedure, 
+    CausalModel, WorkingMemoryController, ParameterOptimizer, 
+    TransferLearningOptimizer
 )
+
+# Import specialized modules
 from .chunk_selection import ContextAwareChunkSelector
 from .generalization import ProceduralChunkLibrary
-from .manager import ProceduralMemoryManager
+from .execution import (
+    ExecutionStrategy, DeliberateExecutionStrategy, 
+    AutomaticExecutionStrategy, AdaptiveExecutionStrategy,
+    StrategySelector
+)
+from .learning import ObservationLearner, ProceduralMemoryConsolidator
+from .temporal import TemporalNode, TemporalProcedureGraph, ProcedureGraph
 
-# Import enhanced components
-from .enhanced import (
-    HierarchicalProcedure, ObservationLearner, CausalModel,
-    ExecutionStrategy, TemporalProcedureGraph, ProcedureGraph,
-    EnhancedProceduralMemoryManager
+# Import manager
+from .manager import (
+    ProceduralMemoryManager, 
+    EnhancedProceduralMemoryManager,
+    demonstrate_cross_game_transfer, 
+    demonstrate_procedural_memory
 )
 
 # Import function tools
@@ -27,72 +38,35 @@ from .function_tools import (
     refine_step
 )
 
-# Demonstration functions
-from .manager import demonstrate_cross_game_transfer, demonstrate_procedural_memory
-
 # Version
 __version__ = "0.1.0"
 
-from .models import (
-    HierarchicalProcedure, 
-    ObservationLearner, 
-    CausalModel, 
-    TemporalNode, 
-    TemporalProcedureGraph,
-    ProcedureGraph,
-    WorkingMemoryController,
-    ParameterOptimizer,
-    TransferLearningOptimizer
-)
-
-from .execution import (
-    ExecutionStrategy,
-    DeliberateExecutionStrategy,
-    AutomaticExecutionStrategy,
-    AdaptiveExecutionStrategy,
-    StrategySelector
-)
-
-from .learning import (
-    ObservationLearner,
-    CausalModel,
-    ProceduralMemoryConsolidator
-)
-
-from .temporal import (
-    TemporalNode,
-    TemporalProcedureGraph,
-    ProcedureGraph
-)
-
-from .manager import (
-    EnhancedProceduralMemoryManager
-)
-
 __all__ = [
-    # Models
-    'HierarchicalProcedure',
-    'ObservationLearner',
-    'CausalModel',
-    'TemporalNode',
-    'TemporalProcedureGraph',
-    'ProcedureGraph',
-    'WorkingMemoryController',
-    'ParameterOptimizer',
+    # Core models
+    'ActionTemplate', 'ChunkTemplate', 'ContextPattern', 'ChunkPrediction',
+    'ControlMapping', 'ProcedureTransferRecord', 'Procedure', 'StepResult',
+    'ProcedureStats', 'TransferStats', 'HierarchicalProcedure',
+    'CausalModel', 'WorkingMemoryController', 'ParameterOptimizer',
     'TransferLearningOptimizer',
     
-    # Execution
-    'ExecutionStrategy',
-    'DeliberateExecutionStrategy',
-    'AutomaticExecutionStrategy',
-    'AdaptiveExecutionStrategy',
-    'StrategySelector',
+    # Specialized components
+    'ContextAwareChunkSelector', 'ProceduralChunkLibrary',
+    'ExecutionStrategy', 'DeliberateExecutionStrategy',
+    'AutomaticExecutionStrategy', 'AdaptiveExecutionStrategy',
+    'StrategySelector', 'ObservationLearner', 'ProceduralMemoryConsolidator',
+    'TemporalNode', 'TemporalProcedureGraph', 'ProcedureGraph',
     
-    # Learning
-    'ObservationLearner',
-    'CausalModel',
-    'ProceduralMemoryConsolidator',
+    # Managers
+    'ProceduralMemoryManager', 'EnhancedProceduralMemoryManager',
     
-    # Manager
-    'EnhancedProceduralMemoryManager'
+    # Demo functions
+    'demonstrate_cross_game_transfer', 'demonstrate_procedural_memory',
+    
+    # Function tools
+    'add_procedure', 'execute_procedure', 'transfer_procedure',
+    'get_procedure_proficiency', 'list_procedures', 'get_transfer_statistics',
+    'identify_chunking_opportunities', 'apply_chunking',
+    'generalize_chunk_from_steps', 'find_matching_chunks',
+    'transfer_chunk', 'transfer_with_chunking', 'find_similar_procedures',
+    'refine_step'
 ]
