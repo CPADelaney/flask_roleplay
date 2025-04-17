3cd0a9ce5a41b7683a0e024644fff88ad9a207ec
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-04-17 10:26:44 -0700

    Update init_db_script.py

--- init_db_script.py
+++ init_db_script.py
@@ -1,6 +1,7 @@
 # init_db_script.py
+import asyncio
 from db.schema_and_seed import initialize_all_data
 
 if __name__ == "__main__":
-    initialize_all_data()
+    asyncio.run(initialize_all_data())
     print("Database created and default data seeded.")
