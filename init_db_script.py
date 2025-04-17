95095c48346144d73a9b420297ebded9d5932883
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-04-17 10:24:27 -0700

    Update init_db_script.py

--- init_db_script.py
+++ init_db_script.py
@@ -2,5 +2,5 @@
 from db.schema_and_seed import initialize_all_data
 
 if __name__ == "__main__":
-    await initialize_all_data()
+    initialize_all_data()
     print("Database created and default data seeded.")
