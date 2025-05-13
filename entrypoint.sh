053b40e3e4c76d3b88da20dd7aa6511afeeedb60
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-05-13 09:49:56 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 if [ "$SERVICE_TYPE" = "worker" ]; then
     echo "Starting Celery Worker with concurrency=1..."
-    # Use the celery_app explicitly
     exec celery -A tasks.celery_app worker --loglevel=INFO --concurrency=1 -E
 else
     echo "Starting Web Server with SocketIO..."
     PORT=${PORT:-8080}
-    exec hypercorn --bind 0.0.0.0:${PORT:-8080} wsgi:app
+    # Add the --workers=1 flag to ensure a single worker process
+    exec hypercorn --workers 1 --bind 0.0.0.0:${PORT} wsgi:app
 fi
