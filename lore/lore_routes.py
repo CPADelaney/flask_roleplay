fd15f3d2df429da7937bbcf8bb58b8d8747f3be5
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-07-14 10:00:04 -0700

    Update lore_routes.py

--- lore/lore_routes.py
+++ lore/lore_routes.py
@@ -11,7 +11,7 @@ from quart import Blueprint, request, jsonify, session
 from typing import Dict, Any
 
 # Import core lore system
-from lore.lore_system import LoreSystem
+from lore.core.lore_system import LoreSystem
 
 # Import route utilities - these would need to be updated for Quart
 from routes.auth import require_login
