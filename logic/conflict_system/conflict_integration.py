b55e077a1c12871271154d32dcc7fab904333854
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-10-01 09:27:52 -0700

    Update conflict_integration.py

--- logic/conflict_system/conflict_integration.py
+++ logic/conflict_system/conflict_integration.py
@@ -118,7 +118,7 @@ class ConflictSystemIntegration:
                             intensity, player_involvement,
                             created_at
                         ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW())
-                        RETURNING id
+                        RETURNING conflict_id
                     """,
                         self.user_id,
                         self.conversation_id,
